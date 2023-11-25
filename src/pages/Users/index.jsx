de89b92ce0d626c1571815fd6422fedd2cd9a425
udkfer <117325542+udkfer@users.noreply.github.com>
2023-11-25 16:48:06 -0300

    created grid styling

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,27 +1,28 @@
 import { getUsers } from "../../../api/dataApi";
 import { useEffect, useState } from "react";
 import UserCard from "../../components/UserCard";
+import styles from "./index.module.css"
 
 function UsersData() {
-    const [users, setUsers] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-            const data = await getUsers();
-            console.log(data);
-            setUsers(data);
-        }
-        fetchData();
-    }, []);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getUsers();
+      console.log(data);
+      setUsers(data);
+    }
+    fetchData();
+  }, []);
 
-    return (
-        <div>
-            {users.map((user) => (
-                <div key={user.id}>
-                    <UserCard user={user} />
-            </div>
-            ))}
+  return (
+    <div className={ styles.user_card }>
+      {users.map((user) => (
+        <div key={user.id}>
+          <UserCard user={user} />
         </div>
-    )
+      ))}
+    </div>
+  )
 }
 
 export default UsersData
